3d0bb6a02a528018f39e01d618cfe60f104a3e37
Eugene <pbody23d@gmail.com>
2026-08-07 11:51:25 +0300

    fix(visor-core): PostChainMixin handler signature for 1.21.1

--- visor-core/src/main/java/org/vmstudio/visor/mixin/client/renderer/PostChainMixin.java
+++ visor-core/src/main/java/org/vmstudio/visor/mixin/client/renderer/PostChainMixin.java
@@ -8,7 +8,7 @@ import org.vmstudio.visor.core.client.render.target.MultiCameraRenderTarget;
 import net.minecraft.client.renderer.PostChain;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceProvider;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -35,7 +35,7 @@ public class PostChainMixin {
 
     @Inject(method = "<init>", at = @At(value = "TAIL"))
     private void visor$onInit(TextureManager textureManager,
-                              ResourceManager resourceManager,
+                              ResourceProvider resourceManager,
                               RenderTarget screenTarget,
                               ResourceLocation name,
                               CallbackInfo ci) throws IOException {
